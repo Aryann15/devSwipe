ca498d8ff2b20e1221669c1d6890e12bcd0ea05a
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-04 23:59:59 +0530

    sign up route

--- server/index.js
+++ server/index.js
@@ -3,52 +3,44 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 const port = 8000;
+const jwt = require('jsonwebtoken');
 const mongoose  = require ('mongoose')
 app.use(cors());
 dotenv.config();
 
-
+app.use(express.json());   
 const secretKey = "Hack_to_the_future"
 
-
-//mongoose schemas
-
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
-  });
-  
-  const adminSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-  });
 
 mongoose.connect(process.env.mongodb_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
 
-
-
+USERS = []
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+
+
 app.post("/user/signup", async (req, res) => {
 const { username, password } = req.body;
-  const admin = ADMINS.find(a => a.username === username);
-  console.log("admin signup");
-  if (admin) {
+  const user = USERS.find(a => a.username === username);
+  if (user) {
     res.status(403).json({ message: 'Admin already exists' });
   } else {
     const newAdmin = { username, password };
-    ADMINS.push(newAdmin);
+    USERS.push(newUser);
     const token = jwt.sign({ username, role: 'admin' },secretKey, { expiresIn: '1h' });
     res.json({ message: 'Admin created successfully', token });
-
 }})
+
+
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
