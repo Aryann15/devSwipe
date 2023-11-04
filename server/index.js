ef940cd08853a185c42563ec84d513cd049dac81
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-04 22:41:07 +0530

    mongodb connect

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 const port = 8000;
-
-// Middleware
+const mongoose  = require ('mongoose')
 app.use(cors());
+dotenv.config();
 
+
+mongoose.connect(process.env.mongodb_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  
 // Routes
-app.get('/', (req,res) => {
-    res.send("Hello world")
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
+
+app.post("/user/signup", async (req, res) => {});
 
 // Start the server
 app.listen(port, () => {
