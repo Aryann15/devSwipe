2df82d24b17be7e768dac2afa554ff09689a3b10
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-05 10:07:58 +0530

    readme api url updated

--- server/scrap.py
+++ server/scrap.py
@@ -52,7 +52,7 @@ else:
 
 
 
-readme_api_url = f'https://api.github.com/repos/{username}/readme'
+readme_api_url = f'https://api.github.com/repos/{username}/{username}/readme'
 readme_response = requests.get(readme_api_url)
 
 if readme_response.status_code == 200:
