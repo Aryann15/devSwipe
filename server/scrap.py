bc0e84f789672e743a0526090234de0a12ff6dbf
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-05 09:25:28 +0530

    github profile

--- server/scrap.py
+++ server/scrap.py
@@ -8,6 +8,6 @@ api_url = f'https://api.github.com/users/{username}'
 response = requests.get(api_url)
 if response.status_code == 200:
     user_data = response.json()
-    print("succes")
+    print(f"User Profile for {username}:")
 else:
     print("Failed to fetch")
