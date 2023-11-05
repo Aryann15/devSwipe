b57bf7841793ce37a5aadb6913f3317668a4562b
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-05 10:12:03 +0530

    creating a empty array to store the user repo in desc =ing order first 25

--- server/scrap.py
+++ server/scrap.py
@@ -25,16 +25,16 @@ params = {
     'direction': 'desc'
 }
 repo_response = requests.get(repo_api_url, params=params)
-
+user_repositories = []
 if repo_response.status_code == 200:
     repos = repo_response.json()[:25]  # Get the first 25 repositories
     for repo in repos:
-        repo_name = repo['name']
-        repo_description = repo['description']
-        repo_fork = repo['fork']
-        print(f"Repository Name: {repo_name}")
-        print(f"Description: {repo_description}")
-        print(f"Fork: {repo_fork}")
+        repo_data = {
+            "name": repo['name'],
+            "description": repo['description'],
+            "fork": repo['fork']
+        }
+        user_repositories.append(repo_data)
 else:
     print(f"Failed to fetch repositories. Status code: {repo_response.status_code}")
 
