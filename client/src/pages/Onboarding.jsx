7f1a8f4c61ad4398ae78c74fc3c2100eeac14d0d
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-24 03:41:49 +0530

    updatd

--- client/src/pages/Onboarding.jsx
+++ client/src/pages/Onboarding.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -6,11 +6,11 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import OutlinedInput from "@mui/material/OutlinedInput";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -23,7 +23,7 @@ const MenuProps = {
   },
 };
 
-const programming_languages = [
+const programmingLanguages = [
   "Javascript",
   "TypeScript",
   "Python",
@@ -39,67 +39,84 @@ const programming_languages = [
   "SQL",
 ];
 
+const skills = ["project management", "design"];
+const techFields = ["front-end Development", "back-end Development", "Dev-ops"];
+
 const Onboarding = () => {
-  const [personName, setPersonName] = React.useState([]);
-  const handleChange = (event) => {
+  const [personName, setPersonName] = useState([]);
+  const [age, setAge] = useState("");
+
+  const handleSkills = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleLanguage = (event) => {
     const {
       target: { value },
     } = event;
     setPersonName(typeof value === "string" ? value.split(",") : value);
   };
+
   return (
-    <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt="logo"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Welcome to DevSwipe
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Please fill out the form
-          </Typography>
-        </CardContent>
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Age" variant="outlined" />
-        <TextField id="outlined-basic" label="City" variant="outlined" />
-        <TextField id="outlined-basic" label="Goals" variant="outlined" />
-        <TextField
-          id="outlined-basic"
-          label="Profile picture link"
-          variant="outlined"
-        />
-        <TextField id="outlined-basic" label="About me" variant="outlined" />
-        <TextField id="outlined-basic" label="Experience" variant="outlined" />
-        <TextField id="outlined-basic" label="github" variant="outlined" />
-        <TextField id="outlined-basic" label="linkedin" variant="outlined" />
-        <FormControl sx={{ m: 1, width: 300 }}>
-          <InputLabel id="demo-multiple-name-label">Name</InputLabel>
-          <Select
-            labelId="demo-multiple-name-label"
-            id="demo-multiple-name"
-            multiple
-            value={personName}
-            onChange={handleChange}
-            input={<OutlinedInput label="Name" />}
-            MenuProps={MenuProps}
-          >
-            {programming_languages.map((name) => (
-              <MenuItem key={name} value={name}>
-                {name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <CardActions>
-          <Button size="small">Submit</Button>
-        </CardActions>
-      </Card>
-    </>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="logo"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Welcome to DevSwipe
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Please fill out the form
+        </Typography>
+      </CardContent>
+      <TextField id="outlined-basic" label="Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Age" variant="outlined" />
+      {/* Add other input fields */}
+      <FormControl sx={{ m: 1, width: 250 }}>
+        <InputLabel id="programming-languages-label">
+          Programming Languages
+        </InputLabel>
+        <Select
+          labelId="programming-languages-label"
+          id="programming-languages"
+          multiple
+          value={personName}
+          onChange={handleLanguage}
+          input={<OutlinedInput label="Programming Languages" />}
+          MenuProps={MenuProps}
+        >
+          {programmingLanguages.map((name) => (
+            <MenuItem key={name} value={name}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <br />
+      <FormControl>
+        <InputLabel id="skills-label">Skills</InputLabel>
+        <Select
+          labelId="skills-label"
+          id="skills"
+          value={age}
+          onChange={handleSkills}
+          autoWidth
+          label="Skills"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {/* Add other skill options */}
+        </Select>
+      </FormControl>
+      <CardActions>
+        <Button size="small">Submit</Button>
+      </CardActions>
+    </Card>
   );
 };
 
