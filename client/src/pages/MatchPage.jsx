3471f882ff91bf3875559739827e8658f710fcab
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-25 04:52:55 +0530

    receiving from the backend, halde connexction

--- client/src/pages/MatchPage.jsx
+++ client/src/pages/MatchPage.jsx
@@ -4,6 +4,19 @@ import TinderCard from "react-tinder-card";
 
 const  MatchPage = () => {
 
+  const [recommendations, setRecommendations] = useState([]);
+  const userId = 1;
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:5000/api/recommendations?id=${userId}`)
+      .then((response) => response.text()) 
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error fetching recommendations:', error); 
+      });
+  }, [userId]);
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing: " + nameToDelete);
