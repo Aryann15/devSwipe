e1b6caab93dfe4bd0455c172d3952f8448ffc63e
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-25 04:51:58 +0530

    importing react tinder cards and

--- client/src/pages/MatchPage.jsx
+++ client/src/pages/MatchPage.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import TinderCard from "react-tinder-card";
+
 
 const  MatchPage = () => {
+
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen");
+  };
+
   return (
     <div> MatchPage</div>
   )
