34cfed5b088f4873b7a9fd9e1356aa363d0dd590
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-24 08:00:42 +0530

    preprocessing

--- server/recommendation.py
+++ server/recommendation.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import string
 import json
+
+def preprocess_text(text):
+    text = text.lower()
+    text = ''.join([char for char in text if char not in string.punctuation])
+    tokens = text.split()
+    return ' '.join(tokens)
