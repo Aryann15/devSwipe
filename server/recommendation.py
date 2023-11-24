2ee4446889a676ea5e9e0473fbabc8bb8d11acca
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-24 08:01:35 +0530

    load recommmendation function

--- server/recommendation.py
+++ server/recommendation.py
@@ -7,3 +7,9 @@ def preprocess_text(text):
     text = ''.join([char for char in text if char not in string.punctuation])
     tokens = text.split()
     return ' '.join(tokens)
+
+def load_recommendations(file_path='recommendations.json'):
+    try:
+        with open(file_path, 'r') as file:
+            recommendations = json.load(file)
+        return recommendations
