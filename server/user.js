6978e4bf7c5145e29b8f35a24879741e6a7c9596
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-25 23:47:15 +0530

    read files

--- server/user.js
+++ server/user.js
@@ -9,8 +9,14 @@ const port = 5001;
 const userDetailsPath = 'recommendation.json';
 let userDetailsData = {};
 
+try {
+  const data = fs.readFileSync(userDetailsPath, 'utf8');
+  userDetailsData = JSON.parse(data);
+} catch (error) {
+  console.error('Error reading user details:', error);
+}
 
-console.log(userDetailsData)
+// console.log(userDetailsData)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
