619e1328b5178ab136b4ac7ec2e9c251285903b2
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-25 23:46:51 +0530

    taking the data from the mimiced db

--- server/user.js
+++ server/user.js
@@ -6,6 +6,11 @@ const cors = require('cors');  // Import cors module
 const app = express();
 const port = 5001;
 
+const userDetailsPath = 'recommendation.json';
+let userDetailsData = {};
+
+
+console.log(userDetailsData)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
