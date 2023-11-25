e32531f2d7f9a13b39e33a3247c6dcd839962022
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-25 23:48:00 +0530

    Route to get user details based on IDs sent from the frontend

--- server/user.js
+++ server/user.js
@@ -16,6 +16,23 @@ try {
   console.error('Error reading user details:', error);
 }
 
+app.use(bodyParser.json());
+app.post('/api/userDetails', (req, res) => {
+  const { userIds } = req.body;
+
+  if (!userIds || !Array.isArray(userIds)) {
+    return res.status(400).json({ error: 'Invalid user IDs' });
+  }
+
+  const userDetails = userIds.map((userId) => {
+    const user = userDetailsData[userId];
+    return user ? { id: userId, name: user.name, age: user.age 
+} : null;
+  });
+
+  res.json(userDetails);
+});
+
 // console.log(userDetailsData)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
