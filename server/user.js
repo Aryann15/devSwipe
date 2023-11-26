6e48efb4ceeaae96f9c64608dde15f1e6b8dc231
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-26 07:44:49 +0530

    Userdetails endpoint

--- server/user.js
+++ server/user.js
@@ -19,17 +19,20 @@ app.use(cors());
 app.use(bodyParser.json());
 app.post('/api/userDetails', (req, res) => {
   const { userIds } = req.body;
-
   if (!userIds || !Array.isArray(userIds)) {
     return res.status(400).json({ error: 'Invalid user IDs' });
   }
 
   const userDetails = userIds.map((userId) => {
     const user = userDetailsData[userId];
-    return user ? { id: userId, name: user.name, age: user.age 
+    return user ? { id: userId, name: user.name,
+         age: user.age , 
+         profession : user.profession,
+         techField : user.techField,
+         profilePicture : user.profilePicture,
+         aboutme : user.aboutme,
 } : null;
   });
-
   res.json(userDetails);
 });
 
