e58c63b3569dc215eda83b3b3b7b16628303d622
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-26 10:57:57 +0530

    adding image

--- client/src/pages/MatchPage.jsx
+++ client/src/pages/MatchPage.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
+import "./MatchPage.css";
 
 const MatchPage = () => {
   const [recArr, setRecArr] = useState([]);
   const userId = 1;
-  const arr = [];
+  const [people, setPeople] = useState([]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen");
+  };
 
   useEffect(() => {
     // Fetch user recommendations
@@ -12,38 +21,57 @@ const MatchPage = () => {
       .then((response) => response.json())
       .then((userIds) => {
         // Fetch user details based on recommendations
-        fetch('http://127.0.0.1:5001/api/userDetails', {
-          method: 'POST',
+        fetch("http://127.0.0.1:5001/api/userDetails", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({ userIds }),
         })
           .then((response) => response.json())
           .then((userDetails) => {
-            console.log(userDetails)
+            console.log(userDetails);
             setRecArr(userDetails);
           })
           .catch((error) => {
-            console.error('Error fetching user details:', error);
+            console.error("Error fetching user details:", error);
           });
       })
       .catch((error) => {
-        console.error('Error fetching recommendations:', error);
+        console.error("Error fetching recommendations:", error);
       });
   }, [userId]);
-    return (
-      <div>
-      {recArr && recArr.map((rec, index) => (
-  <React.Fragment key={index}>
-    <h1>{rec && rec.name}</h1>
-    <h2>{rec && rec.age}</h2>
-    <h3>{rec && rec.profession}</h3>
-    <h4>{rec && rec.aboutme}</h4>
-  </React.Fragment>
-))}
-   </div>  
-    )
-  }
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {recArr &&
+          recArr.map((rec, index) => (
+            <TinderCard
+              className="swipe"
+              key={index}
+              onSwipe={(dir) => swiped(dir, rec.name)}
+              onCardLeftScreen={() => outOfFrame(rec.name)}
+            >
+              <div
+                className="card"
+                style={{
+                  backgroundImage: rec ? `url(${rec.profilePicture})` : "none",
+                }}
+              >
+                {rec && (
+                  <>
+                    <h1>{rec.name}</h1>
+                    <h2>{rec.age}</h2>
+                    <h3>{rec.profession}</h3>
+                    <h4>{rec.aboutme}</h4>
+                  </>
+                )}
+              </div>
+            </TinderCard>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default MatchPage;
