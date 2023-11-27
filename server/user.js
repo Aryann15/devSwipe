bf01fcdb99e9c6e04d48448392bf02509c0ad989
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-28 00:24:34 +0530

    getting some onboarding data to rec.json

--- server/user.js
+++ server/user.js
@@ -1,74 +1,85 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require('cors');  
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 5002;
 
-const userDetailsPath = 'recommendation.json';
+const userDetailsPath = "recommendation.json";
 let userDetailsData = {};
 
 try {
-  const data = fs.readFileSync(userDetailsPath, 'utf8');
+  const data = fs.readFileSync(userDetailsPath, "utf8");
   userDetailsData = JSON.parse(data);
 } catch (error) {
-  console.error('Error reading user details:', error);
+  console.error("Error reading user details:", error);
 }
-app.use(cors());  
+app.use(cors());
 app.use(bodyParser.json());
-app.post('/api/userDetails', (req, res) => {
+app.post("/api/userDetails", (req, res) => {
   const { userIds } = req.body;
   if (!userIds || !Array.isArray(userIds)) {
-    return res.status(400).json({ error: 'Invalid user IDs' });
+    return res.status(400).json({ error: "Invalid user IDs" });
   }
   const userDetails = userIds.map((userId) => {
     const user = userDetailsData[userId];
-    return user ? { id: userId,
-         name: user.name,
-         age: user.age , 
-         profession : user.profession,
-         techFields : user.techFields,
-         profilePicture : user.profilePicture,
-         aboutme : user.aboutme,
-} : null;
+    return user
+      ? {
+          id: userId,
+          name: user.name,
+          age: user.age,
+          profession: user.profession,
+          techFields: user.techFields,
+          profilePicture: user.profilePicture,
+          aboutme: user.aboutme,
+        }
+      : null;
   });
   res.json(userDetails);
 });
 
+let recommendations = JSON.parse(
+  fs.readFileSync("./recommendation.json", "utf-8")
+);
 
-let recommendations = JSON.parse(fs.readFileSync('./recommendation.json', 'utf-8'));
+app.post("/signup", (req, res) => {
+  const newUser = req.body;
 
-app.post('/signup', (req, res) => {
-    const newUser = req.body;
-  
-    
-    const existingUser = recommendations.find(user => user.username === newUser.username);
-    if (existingUser) {
-      return res.status(403).json({ message: 'User already exists' });
-    }
-  
-    newUser.id = recommendations.length + 1;
+  const existingUser = recommendations.find(
+    (user) => user.username === newUser.username
+  );
+  if (existingUser) {
+    return res.status(403).json({ message: "User already exists" });
+  }
+  newUser.id = recommendations.length + 1;
 
-    recommendations.push(newUser);
+  recommendations.push(newUser);
+  fs.writeFileSync(
+    "./recommendation.json",
+    JSON.stringify(recommendations, null, 2)
+  );
+
+  res.json({ message: "User created successfully", user: newUser });
+});
+
+app.post("/onboarding", (req, res) => {
+  console.log("received post req onboarding");
+  const { userId, selectedGoals, selectedSkills } = req.body;
   
-    fs.writeFileSync('./recommendation.json', JSON.stringify(recommendations, null, 2));
-  
-    // Return the newly created user
-    res.json({ message: 'User created successfully', user: newUser });
+  userToUpdate.selectedGoals = selectedGoals;
+  userToUpdate.selectedSkills = selectedSkills;
+
+  fs.writeFileSync(
+    "./recommendation.json",
+    JSON.stringify(recommendations, null, 2)
+  );
+
+  res.json({
+    message: "Onboarding data updated successfully",
+    user: userToUpdate,
   });
-
-
-  app.post('/onboarding', (req, res) => {
-    console.log("received post req onboarding")
-    const {userId, selectedGoals, selectedSkills } = req.body;
-    
-   
-    fs.writeFileSync('./recommendation.json', JSON.stringify(recommendations, null, 2));
-  
-    res.json({ message: 'Onboarding data updated successfully', user: userToUpdate });
-  });
-
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
