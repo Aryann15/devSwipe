c40bbbc0da43e9181ad6306db2373a0b2fe18312
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-27 21:26:28 +0530

    signupUse function

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -11,6 +11,28 @@ const SignupPage = () => {
    
   });
 
+  const signupUser = async (userData) => {
+    try {
+      const response = await fetch('http://localhost:5001/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+  
+      if (!response.ok) {
+       
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
