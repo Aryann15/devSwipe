32969508ca19bb508c469fae945dd560c071e68f
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-27 21:22:25 +0530

    signup page init

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,37 @@
+
+import React, { useState } from 'react';
+
+const SignupPage = () => {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleSignup}>
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
