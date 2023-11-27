ddafdfc8f8c16bb29a92393d4fb2995c8ded960f
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-28 02:45:27 +0530

    updated onbardng page

--- client/src/pages/Onboarding.jsx
+++ client/src/pages/Onboarding.jsx
@@ -10,9 +10,8 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { useState } from "react";
-import './Onboarding.css'
-import hero from "../assets/Hero.png"
-
+import "./Onboarding.css";
+import { useNavigate } from "react-router-dom";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -42,73 +41,73 @@ const programming_languages = [
 ];
 
 const goals = [
-    "Contribute to open-source projects",
-    "Participate in hackathons",
-    "Build a professional network",
-    "Learn new technologies",
-    "Collaborate with other developers",
-    "Showcase personal projects",
-    "Stay updated on industry trends",
-  ];
+  "Contribute to open-source projects",
+  "Participate in hackathons",
+  "Build a professional network",
+  "Learn new technologies",
+  "Collaborate with other developers",
+  "Showcase personal projects",
+  "Stay updated on industry trends",
+];
 
 const skills = [
-    "project management",
-    "design",
-    "data analysis",
-    "UI/UX design",
-    "agile methodology",
-    "team collaboration",
-  ];
-  const fields = [
-    "front-end Development",
-    "back-end Development",
-    "Dev-ops",
-    "database administration",
-    "cloud computing",
-    "security",
-    "mobile app development",
-    "full-stack development",
-    "networking",
-    "game development",
-    "blockchain",
-    "data science",
-    "testing and QA",
-    "AI and machine learning",
-    "UI/UX design",
-    "project management",
-    "system administration",
-    "business analysis",
-    "IT consulting",
-  ];
+  "project management",
+  "design",
+  "data analysis",
+  "UI/UX design",
+  "agile methodology",
+  "team collaboration",
+];
+const fields = [
+  "front-end Development",
+  "back-end Development",
+  "Dev-ops",
+  "database administration",
+  "cloud computing",
+  "security",
+  "mobile app development",
+  "full-stack development",
+  "networking",
+  "game development",
+  "blockchain",
+  "data science",
+  "testing and QA",
+  "AI and machine learning",
+  "UI/UX design",
+  "project management",
+  "system administration",
+  "business analysis",
+  "IT consulting",
+];
 const experienceLevels = ["0-1", "2-5", "5+", "8+"];
 
 const professions = ["Student", "Full-time Employee", "Part-time Employee"];
 
 const Onboarding = () => {
+  const navigate = useNavigate();
   const [language, setLanguage] = React.useState([]);
   const [selectedSkills, setSelectedSkills] = useState([]);
   const [field, setFields] = useState([]);
   const [experience, setExperience] = useState("");
   const [profession, setProfession] = useState("");
-  const [selectedGoals,setSelectedGoals] = useState([])
-  const [name,setName] = useState ("")
-  const [age,setAge] = useState ()
-  const [city,setCity] = useState ("")
-  const [picture,setPicture] = useState ("")
-  const [aboutme,setAboutme] = useState ("")
-  const [github,setGithub] = useState ("")
-  const [linkedin,setLinkedin] = useState ("")
-
+  const [selectedGoals, setSelectedGoals] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState();
+  const [city, setCity] = useState("");
+  const [picture, setPicture] = useState("");
+  const [aboutme, setAboutme] = useState("");
+  const [github, setGithub] = useState("");
+  const [linkedin, setLinkedin] = useState("");
 
   const handleOnboardingSubmit = async () => {
     try {
       const queryParams = new URLSearchParams(window.location.search);
-const userId = queryParams.get('userId');
+      const userId = queryParams.get("userId");
 
-      const response = await fetch('http://localhost:5002/onboarding', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5002/onboarding", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           userId,
@@ -123,29 +122,30 @@ const userId = queryParams.get('userId');
           selectedGoals,
           selectedSkills,
           field,
-          experience, 
-          profession
+          experience,
+          profession,
         }),
       });
-  
+
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
-  
+
       const data = await response.json();
-      console.log(data.message); // Log success message
+      console.log(data.message);
+      navigate(`/matchpage?userId=${userId}`);
     } catch (error) {
-      console.error('Onboarding failed', error);
+      console.error("Onboarding failed", error);
     }
   };
-  
+
   const handleName = (event) => {
     setName(event.target.value);
   };
   const handleAge = (event) => {
     setAge(event.target.value);
   };
-  const handleCity= (event) => {
+  const handleCity = (event) => {
     setCity(event.target.value);
   };
   const handleAboutme = (event) => {
@@ -183,14 +183,11 @@ const userId = queryParams.get('userId');
     setLanguage(typeof value === "string" ? value.split(",") : value);
   };
   const handleGoal = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setSelectedGoals(typeof value === "string" ? value.split(",") : value);
+    setSelectedGoals(event.target.value);
   };
+
   return (
     <div className="CardContainer">
-    
       <div className="onboarding-card">
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
@@ -213,7 +210,6 @@ const userId = queryParams.get('userId');
           label="Age"
           variant="outlined"
           onChange={handleAge}
-
         />
         <TextField
           className="TextField"
@@ -221,16 +217,14 @@ const userId = queryParams.get('userId');
           label="City"
           variant="outlined"
           onChange={handleCity}
-
         />
-        
+
         <TextField
           className="TextField"
           id="outlined-basic"
           label="Profile picture link"
           variant="outlined"
           onChange={handlePicture}
-
         />
         <TextField
           className="TextField"
@@ -238,16 +232,14 @@ const userId = queryParams.get('userId');
           label="About me"
           variant="outlined"
           onChange={handleAboutme}
-
         />
-        
+
         <TextField
           className="TextField"
           id="outlined-basic"
           label="github"
           variant="outlined"
           onChange={handleGithub}
-
         />
         <TextField
           className="TextField"
@@ -277,10 +269,8 @@ const userId = queryParams.get('userId');
           </Select>
         </FormControl>
 
-        <FormControl className="FormControl" >
-          <InputLabel id="demo-multiple-name-label">
-            Your Goals{" "}
-          </InputLabel>
+        {/* <FormControl className="FormControl">
+          <InputLabel id="demo-multiple-name-label">Your Goals </InputLabel>
           <Select
             labelId="demo-multiple-name-label"
             id="demo-multiple-name"
@@ -296,9 +286,25 @@ const userId = queryParams.get('userId');
               </MenuItem>
             ))}
           </Select>
-        </FormControl>
+        </FormControl> */}
 
-        <FormControl className="FormControl" >
+        <FormControl className="FormControl">
+          <InputLabel id="profession-label">Your Goals!</InputLabel>
+          <Select
+            labelId="goal-label"
+            id="goal"
+            value={selectedGoals}
+            onChange={handleGoal}
+            label="Goal"
+          >
+            {goals.map((goal) => (
+              <MenuItem key={goal} value={goal}>
+                {goal}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className="FormControl">
           <InputLabel id="skills-label">Skills</InputLabel>
           <Select
             labelId="skills-label"
@@ -316,7 +322,7 @@ const userId = queryParams.get('userId');
             ))}
           </Select>
         </FormControl>
-        <FormControl className="FormControl" >
+        <FormControl className="FormControl">
           <InputLabel id="tech_fields-label">Tech Fields</InputLabel>
           <Select
             labelId="tech_field-label"
@@ -325,7 +331,7 @@ const userId = queryParams.get('userId');
             value={field}
             onChange={handleFields}
             label="Tech Fields"
-             MenuProps={MenuProps}
+            MenuProps={MenuProps}
           >
             {fields.map((field) => (
               <MenuItem key={field} value={field}>
@@ -334,7 +340,7 @@ const userId = queryParams.get('userId');
             ))}
           </Select>
         </FormControl>
-        <FormControl className="FormControl" >
+        <FormControl className="FormControl">
           <InputLabel id="experience-label">Experience</InputLabel>
           <Select
             labelId="experience-label"
@@ -350,7 +356,7 @@ const userId = queryParams.get('userId');
             ))}
           </Select>
         </FormControl>
-        <FormControl className="FormControl" >
+        <FormControl className="FormControl">
           <InputLabel id="profession-label">Profession</InputLabel>
           <Select
             labelId="profession-label"
@@ -369,12 +375,17 @@ const userId = queryParams.get('userId');
         <br />
         <br />
         <CardActions>
-          <Button className="Button" size="medium" variant="contained" onClick={handleOnboardingSubmit}>
+          <Button
+            className="Button"
+            size="medium"
+            variant="contained"
+            onClick={handleOnboardingSubmit}
+          >
             Submit
           </Button>
         </CardActions>
       </div>
-      </div>
+    </div>
   );
 };
 
