b848e987391343d9eac358651653c3ec0a74d46a
Raj Aryan Singh <rajlysm15@gmail.com>
2023-12-04 20:00:26 +0530

    signup page page modified

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 
 const SignupPage = () => {
   const navigate = useNavigate();
@@ -50,33 +60,69 @@ const SignupPage = () => {
 
   return (
     <div>
-      <h1>Signup Page</h1>
-      <form>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-
-        <br />
-        <button type="button" onClick={handleSignup}>
-          Signup
-        </button>
-      </form>
+       <Container component="main" maxWidth="xs">
+      <Box
+          sx={{
+            backgroundColor:"white",
+            marginTop: 24,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            borderRadius:10
+          }}
+        >
+      <Typography component="h1" variant="h" padding={5}>
+            Sign up
+          </Typography>
+      <Grid container spacing={2}
+      borderRadius={10}>
+              <Grid item xs={13} sm={14}></Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  // fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  // fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+               
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              // fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              size="large"
+              onClick={handleSignup}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="center" padding={3}>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+      </Box>
+      </Container>
     </div>
   );
 };
