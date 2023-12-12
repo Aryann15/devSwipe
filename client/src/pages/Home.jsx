e045aa22c2951b1fb1bb9a7b9e704b4e8c5a0100
Raj Aryan Singh <rajlysm15@gmail.com>
2023-12-13 03:32:43 +0530

    fetch user dwtails for requesters

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 const ConnectionsPage = () => {
   const userId = 95
+  const [recData ,setRecData] = useState ([])
   const [connections, setConnections] = useState([]);
   useEffect(() => {
     fetch(`http://127.0.0.1:5001/connections/${userId}`)
@@ -11,10 +12,25 @@ const ConnectionsPage = () => {
       .then((connectionsData) => {
         console.log(connectionsData)
         setConnections(connectionsData);
+        fetch("http://127.0.0.1:5001/api/userDetails", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userIds}),
+        })
+          .then((response) => response.json())
+          .then((userDetails) => {
+            console.log(userDetails);
+            setRecArr(userDetails);
+          })
+          .catch((error) => {
+            console.error("Error fetching user details:", error);
+          });
       })
       .catch((error) => {
-        console.error("Error fetching connections:", error);
-      });
+        console.error("Error fetching recommendations:", error);
+      });      
   }, [userId]);
   
   
