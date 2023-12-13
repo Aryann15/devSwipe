94865f6873f50230c5932e7429df3e3db6f0013b
Raj Aryan Singh <rajlysm15@gmail.com>
2023-12-14 00:14:53 +0530

    fetch projects form backend

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -3,7 +3,15 @@ import React, { useState, useEffect } from "react";
 const Project = () => {
   const [projects, setProjects] = useState([]);
 
-  
+  useEffect(() => {
+    // Fetch projects from the backend
+    fetch("http://127.0.0.1:5001/projects")
+      .then((response) => response.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.error("Error fetching projects:", error));
+  }, []);
+
+ 
   return (
     <div>
       <h1>View Projects</h1>
@@ -25,8 +33,7 @@ const Project = () => {
       <div className="leaderboard">
       <h2>Leaderboard</h2>
       <ul>
-        
-      </ul>
+              </ul>
     </div>
     </div>
   );
