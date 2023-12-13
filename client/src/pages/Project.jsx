38abda9317b6f930c8533dbce1cc047b1e39682f
Raj Aryan Singh <rajlysm15@gmail.com>
2023-12-14 05:26:21 +0530

    dont allow duplicate likes for same ids

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,7 +4,6 @@ const Project = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    // Fetch projects from the backend
     fetch("http://127.0.0.1:5001/projects")
       .then((response) => response.json())
       .then((data) => setProjects(data))
@@ -12,66 +11,62 @@ const Project = () => {
   }, []);
 
   const handleLike = (projectId) => {
-    // Check if the user has already liked this project
-    const likedProjects = JSON.parse(localStorage.getItem("likedProjects")) || [];
-    if (likedProjects.includes(projectId)) {
-      console.log("Already liked this project");
-      return;
-    }
+    
+    const userId = 13; // Replace with your actual user ID
 
-    // Update local state
-    const updatedProjects = projects.map((project) =>
-      project.id === projectId ? { ...project, likes: project.likes + 1 } : project
-    );
-    setProjects(updatedProjects);
-
-    // Update liked projects in local storage
-    localStorage.setItem("likedProjects", JSON.stringify([...likedProjects, projectId]));
-
-    // Send the like to the backend
-    fetch("http://127.0.0.1:5001/projects/like", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ projectId }),
+    fetch(`http://127.0.0.1:5001/projects/like?userId=${userId}&projectId=${projectId}`, {
+      method: 'POST',
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data.message))
-      .catch((error) => console.error("Error updating project like:", error));
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Already liked');
+      }
+    })
+    .then((data) => {
+      // Update the projects state with the new likes count
+      setProjects((prevProjects) =>
+        prevProjects.map((project) =>
+          project.id === projectId ? { ...project, likes: data.likesCount } : project
+        )
+      );
+    })
+    .catch((error) => {
+      console.error('Error liking project:', error);
+      alert('You already liked this project');
+    });
   };
+  
 
   return (
     <div>
       <h1>View Projects</h1>
       <div className="leaderboard">
         {/* Leaderboard stats (purple box) */}
-        <div className="leaderboard-stats" style={{ backgroundColor: "purple" }}>
-         
-        </div>
+        <div className="leaderboard-stats" style={{ backgroundColor: "purple" }}></div>
       </div>
       {projects.map((project) => (
         <div key={project.id} className="project">
           <h3>{project.title}</h3>
-          <p>Likes: {project.likes}</p>
+          <p>Likes: {project.likes.count}</p>
           <button onClick={() => handleLike(project.id)}>Like</button>
         </div>
       ))}
 
-
       <div className="leaderboard">
-      <h2>Leaderboard</h2>
-      <ul>
-        {projects
-          .sort((a, b) => b.likes - a.likes) // Sort projects by likes in descending order
-          .map((project) => (
-            <li key={project.id}>
-              <span className="project-title">{project.title +" =  "}</span>
-              <span className="like-count">{project.likes} Likes</span>
-            </li>
-          ))}
-      </ul>
-    </div>
+        <h2>Leaderboard</h2>
+        <ul>
+          {projects
+            .sort((a, b) => b.likes - a.likes) // Sort projects by likes in descending order
+            .map((project) => (
+              <li key={project.id}>
+                <span className="project-title">{project.title + "    "}</span>
+                <span className="like-count">{project.likes.count} Likes</span>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
