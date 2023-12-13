9d38529940763c29ab9ebd5bb1a84689b1a734c1
Raj Aryan Singh <rajlysm15@gmail.com>
2023-12-14 00:12:33 +0530

    projects display

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
-}
+  const [projects, setProjects] = useState([]);
 
-export default Project
+  
+  return (
+    <div>
+      <h1>View Projects</h1>
+      <div className="leaderboard">
+        {/* Leaderboard stats (purple box) */}
+        <div className="leaderboard-stats" style={{ backgroundColor: "purple" }}>
+         
+        </div>
+      </div>
+      {projects.map((project) => (
+        <div key={project.id} className="project">
+          <h3>{project.title}</h3>
+          <p>Likes: {project.likes}</p>
+          <button onClick={() => handleLike(project.id)}>Like</button>
+        </div>
+      ))}
+
+
+      <div className="leaderboard">
+      <h2>Leaderboard</h2>
+      <ul>
+        
+      </ul>
+    </div>
+    </div>
+  );
+};
+
+export default Project;
