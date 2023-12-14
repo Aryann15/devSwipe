3d9e922e669ee5120dafc205680595d0ced7c8c7
Raj Aryan Singh <rajlysm15@gmail.com>
2023-12-14 06:22:42 +0530

    home page video added

--- client/src/pages/Hero.jsx
+++ client/src/pages/Hero.jsx
@@ -4,7 +4,10 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="video-background">
-      
+      <video autoPlay muted loop className="video">
+        <source src={hero} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
       <div className="content">
         <h1>Welcome to My Website</h1>
